1be00bd6f085a30c79da395c4156c054dfac71ae
arotena <albertosss27@gmail.com>
2017-07-11 18:20:04 +0200

    Nuevas funciones

---
+++ Funciones.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+class Funciones:
+
+    def dime_Hola(self):
+        return "Hola"
+
+    def menor_5 (self,n):
+        if (n<5):
+            return True
+        else:
+            return False        
---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from Funciones import Funciones
+import unittest
+
+class CarcassoneTest(unittest.TestCase):
+
+    def test_Hola(self):
+        f = Funciones()
+        self.assertEqual("Hola",f.dime_Hola())
+
+    def test_menor_5_True(self):
+        f = Funciones()
+        self.assertTrue(f.menor_5(3))
+
+    def test_menor_5_False(self):
+        f = Funciones()
+        self.assertFalse(f.menor_5(7))
+
+if __name__ == '__main__':
+    unittest.main()
